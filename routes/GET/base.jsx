70314bd920e115cb44338ab757d3949264a739ba
Nils Olofsson <nilsolofsson@live.se>
2019-04-12 21:31:00 +0200

    small stuff

--- routes/GET/base.jsx
+++ routes/GET/base.jsx
@@ -8,7 +8,6 @@ class Base extends React.Component {
                     <title>{this.props.title}</title>
                     <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet"></link>
 
-                    
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
                     <link rel="stylesheet" href="/public/style.css" crossorigin="anonymous" />
 
