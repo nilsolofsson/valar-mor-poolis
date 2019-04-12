ddaa481874dd5f2a8091ed8eeabd4450b47c8dec
Nils Olofsson <nilsolofsson@live.se>
2019-04-12 22:23:35 +0200

    trailing lines...

--- routes/GET/base.jsx
+++ routes/GET/base.jsx
@@ -8,7 +8,7 @@ class Base extends React.Component {
                     <meta charset="utf-8" />
                     <meta http-equiv="x-ua-compatible" content="ie=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
-                
+
                     <title>{this.props.title}</title>
                     <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet"></link>
 
