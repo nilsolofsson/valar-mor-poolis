73fef71bec2a27d5346b338a169ac32ccf5432cb
Nils Olofsson <nilsolofsson@live.se>
2019-04-12 20:14:21 +0200

    added a cache

--- controllers/got-controller.js
+++ controllers/got-controller.js
@@ -1,10 +1,35 @@
+/* eslint-disable no-eq-null */
 class GameOfThrones {
     constructor(config) {
         this.Data = new (require('../data/google.sheets'))(config.sheets);
         this.characters = config.characters;
+
+        this.cache = {};
+        this.cacheTTL = config.cacheTTL;
+    }
+
+    fetchFromCache(object) {
+        if (this.cache[object] != null && this.cache[object].cachedTime <= Date.now()+this.cacheTTL) {
+            return this.cache[object].data;
+        }
+
+        return null;
+    }
+
+    saveToCache(object, data) {
+        this.cache[object] = {
+            cachedTime: Date.now(),
+            data
+        }
     }
 
     async getCharacterStatuses() {
+        let cache = this.fetchFromCache('characters');
+
+        if (cache !== null) {
+            return cache;
+        }
+
         let rows = await this.Data.getRows(1, 1, 40);
 
         let returnArr = [];
@@ -20,10 +45,17 @@ class GameOfThrones {
             });
         });
 
+        this.saveToCache('characters', returnArr);
+
         return returnArr;
     }
 
     async getUserResponseNames() {
+        let cache = this.fetchFromCache('UserResponseNames');
+
+        if (cache !== null) {
+            return cache;
+        }
         let rows = await this.Data.getRows(0, 1, 40);
 
         let returnArr = [];
@@ -34,6 +66,8 @@ class GameOfThrones {
                 points: 0
             })
         });
+
+        this.saveToCache('UserResponseNames', returnArr);
 
         return returnArr;
     }
